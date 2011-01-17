10e08be2f9c4b4ea6c27cef3b0920719f6239d07
Andrew Ardill <andrew.ardill@gmail.com>
2011-01-18 09:56:20 +1100

    added dev commands debug and god.

--- src/main/java/com/cogito/bukkit/dev/DevBukkit.java
+++ src/main/java/com/cogito/bukkit/dev/DevBukkit.java
@@ -2,9 +2,14 @@
 package com.cogito.bukkit.dev;
 
 import java.io.File;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,7 +35,7 @@ public class DevBukkit extends JavaPlugin {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvent(Event.Type.ENTITY_COMBUST, entityListener, Priority.Normal, this);
         pm.registerEvent(Event.Type.ENTITY_DAMAGED, entityListener, Priority.Normal, this);
-        pm.registerEvent(Event.Type.ENTITY_DAMAGEDBY_BLOCK, entityListener, Priority.Normal, this);
+        //pm.registerEvent(Event.Type.ENTITY_DAMAGEDBY_BLOCK, entityListener, Priority.Normal, this);
         pm.registerEvent(Event.Type.ENTITY_DAMAGEDBY_ENTITY, entityListener, Priority.Normal, this);
         pm.registerEvent(Event.Type.ENTITY_DAMAGEDBY_PROJECTILE, entityListener, Priority.Normal, this);
         pm.registerEvent(Event.Type.ENTITY_DEATH, entityListener, Priority.Normal, this);
@@ -38,5 +43,56 @@ public class DevBukkit extends JavaPlugin {
         // EXAMPLE: Custom code, here we just output some info so we can check all is well
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+    
+    public void onCommand(Player player, String command, String[] args) {
+        String[] split = args;
+
+        if (command.equalsIgnoreCase("/dev")) {
+            if (split.length > 1) {
+                if (split[1].equalsIgnoreCase("debug")) {
+                    if (split.length == 2) {
+                        player.sendMessage(ChatColor.RED + "Dev: debug mode toggle.");
+                        entityListener.debugToggle();
+                    } else if (split.length == 3) {
+                        if (split[2].equalsIgnoreCase("on")) {
+                            player.sendMessage(ChatColor.RED + "Dev: debug mode on.");
+                            entityListener.debugOn();
+                        } else if (split[2].equalsIgnoreCase("off")) {
+                            player.sendMessage(ChatColor.RED + "Dev: debug mode off.");
+                            entityListener.debugOn();
+                        } else {
+                            sendUsage(player);
+                        }
+                    }
+                } else if (split[1].equalsIgnoreCase("god")) {
+                    if (split.length == 2) {
+                        player.sendMessage(ChatColor.RED + "Dev: god mode on.");
+                        entityListener.godMode(player, true);
+                    } else if (split.length == 3) {
+                        if (split[2].equalsIgnoreCase("on")) {
+                            player.sendMessage(ChatColor.RED + "Dev: god mode on.");
+                            entityListener.godMode(player, true);
+                        } else if (split[2].equalsIgnoreCase("off")) {
+                            player.sendMessage(ChatColor.RED + "Dev: god mode off.");
+                            entityListener.godMode(player, false);
+                        } else {
+                            sendUsage(player);
+                        }
+                    }
+                } else{
+                    sendUsage(player);
+                }
+            } else {
+                sendUsage(player);
+            }
+
+        } 
+    }
+    
+    public void sendUsage(Player player){
+        player.sendMessage(ChatColor.RED + "Incorrect usage of command /dev. Examples:");
+        player.sendMessage(ChatColor.RED + "/dev debug [on|off] - toggle full debug mode.");
+        player.sendMessage(ChatColor.RED + "/dev god [on|off] - toggle god mode.");
     }
 }
--- src/main/java/com/cogito/bukkit/dev/DevEntityListener.java
+++ src/main/java/com/cogito/bukkit/dev/DevEntityListener.java
@@ -1,6 +1,9 @@
 
 package com.cogito.bukkit.dev;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -9,6 +12,7 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 /**
  * Handle all Player related events
@@ -19,17 +23,27 @@ public class DevEntityListener extends EntityListener {
     private final DevBukkit plugin;
     
     private boolean debug;
+    private Map<Player, Boolean> gods;
 
     public DevEntityListener(DevBukkit instance) {
         plugin = instance;
-        debug = true;
+        debug = false;
+        gods = new HashMap<Player, Boolean>();
     }
     
     public void onEntityDamageByBlock(EntityDamageByBlockEvent event) {
         if(debug){
-            String entityDescriptor = getDescriptor(event.getEntity());
-            System.out.println("An instance of "+entityDescriptor+" was damaged by a block "+event.getDamager().getType().toString());
+            System.out.println(event.getCause() + "("+event.getDamage()+"): "
+                    + event.getEntity().getClass().getSimpleName()
+                    + " was damaged by block "
+                    + event.getDamager().getClass().getSimpleName()
+                    + "."
+                    );
         }
+        if(event.getEntity() instanceof Player){
+            event.setCancelled(true);
+        }
+        
     }
 
     private String getDescriptor(Entity entity) {
@@ -38,14 +52,73 @@ public class DevEntityListener extends EntityListener {
     }
 
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if(debug){
+        System.out.println(
+                event.getCause() + "("+event.getDamage()+"): "
+                + event.getEntity().getClass().getSimpleName()
+                + " was damaged by "
+                + event.getDamager().getClass().getSimpleName()
+                + "."
+                );
+        }
+        if(event.getEntity() instanceof Player){
+            event.setCancelled(true);
+        }
     }
     
     public void onEntityDamageByProjectile(EntityDamageByProjectileEvent event) {
+        if(debug){
+            System.out.println(
+                    event.getCause() + "("+event.getDamage()+"): "
+                    + event.getEntity().getClass().getSimpleName()
+                    + " was damaged by a projectile ("
+                    + event.getDamager().getClass().getSimpleName()
+                    + ")."
+                    );
+            }
+            if(event.getEntity() instanceof Player){
+                event.setCancelled(true);
+            }
     }
     
     public void onEntityCombust(EntityCombustEvent event) {
+        if(debug){
+            System.out.println(
+                    event.getEntity() + ": "
+                    + event.getEntity().getClass().getSimpleName()
+                    + " caught fire."
+                    );
+            }
+            if(event.getEntity() instanceof Player){
+                if(isGod((Player) event.getEntity())){
+                    event.setCancelled(true);
+                }
+            }
     }
     
+    private boolean isGod(Player player) {
+        return gods.containsKey(player);
+    }
+
     public void onEntityDamage(EntityDamageEvent event) {
+    }
+
+    public void debugToggle() {
+        if(debug){
+            debug = false;
+        } else{
+            debug = true;
+        }
+    }
+
+    public void debugOn() {
+        debug = true;
+    }
+    public void debugOff() {
+        debug = false;
+    }
+    
+    public void godMode(Player player, boolean iAmGod){
+        gods.put(player, Boolean.valueOf(iAmGod));
     }
 }
